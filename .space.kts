e8b9a35a618f22e05cf5d224a376be9a98cadb4b
Alexander Nozik <altavir@gmail.com>
2022-05-25 18:40:23 +0300

    Update CD

--- .space.kts
+++ .space.kts
@@ -1,13 +1,14 @@
 job("Deploy") {
-    container(displayName = "Build and install", image = "openjdk:11") {
-        kotlinScript { api ->
-            // here can be your complex logic
-            api.gradlew("installDist")
-        }
+    startOn {
+        gitPush { enabled = false }
     }
-    container(displayName = "Deploy", image = "openjdk:11") {
-        env["HOST"] = Params("sciprog-host")
-        env["ID"] = Secrets("sciprog-webmaster-id")
+
+    gradlew("openjdk:11", "installDist")
+
+    container(displayName = "Deploy via scp", image = "openjdk:11") {
+        env["HOST"] = Params("spc-host")
+        env["USER"] = Secrets("spc-webmaster-user")
+        env["ID"] = Secrets("spc-webmaster-id")
 
         shellScript {
             interpreter = "/bin/bash"
