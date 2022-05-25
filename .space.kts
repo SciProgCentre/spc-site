98ad36d77771c39e825e5d99f67b94930f27b94b
Alexander Nozik <nozik.aa@mipt.ru>
2022-05-25 17:28:59 +0000

    update CD

--- .space.kts
+++ .space.kts
@@ -1,22 +1,25 @@
+import circlet.pipelines.script.put
+
 job("Deploy") {
     startOn {
         gitPush { enabled = false }
     }
-    
+
     container(image = "openjdk:11") {
         kotlinScript { api ->
             api.gradlew("installDist")
+            api.fileShare().put(java.io.File("build/installDist"))
         }
-        
+    }
+
+    container(image = "openjdk:11") {
         env["HOST"] = Params("spc-host")
         env["USER"] = Secrets("spc-webmaster-user")
         env["ID"] = Secrets("spc-webmaster-id")
 
         shellScript {
             interpreter = "/bin/bash"
-            content = """
-                ls -la
-            """.trimIndent()
+            content = "echo \$ID | ssh -i /dev/stdin -r /mnt/space/share/spc-site/ \"\$USER@\$HOST:/opt\""
         }
     }
 }
